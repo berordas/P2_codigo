67e1eba9b623485965cf94ceb0fbd197ff6f1878
berordas <202214793@alu.comillas.edu>
2024-10-03 12:23:30 +0200

    city

--- City.cs
+++ City.cs
@@ -2,5 +2,57 @@
 
 namespace Practice2
 {
+    public class City : IMessageWritter
+    {
+        private List<string> taxiLicenses;  // List of taxi licenses
+        private PoliceStation policeStation;
+
+        // Constructor
+        public City()
+        {
+            taxiLicenses = new List<string>();
+            policeStation = new PoliceStation();
+        }
+
+        // Method to add a new taxi license
+        public void AddLicense(string license)
+        {
+            if (!taxiLicenses.Contains(license))
+            {
+                taxiLicenses.Add(license);
+                Console.WriteLine(WriteMessage($"{license}: has been successfully added."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage($"{license}: is already registered."));
+            }
+        }
+
+        // Method to remove an existing taxi license
+        public void DropLicense(string license)
+        {
+            if (taxiLicenses.Contains(license))
+            {
+                taxiLicenses.Remove(license);
+                Console.WriteLine(WriteMessage($"{license}: has been successfully removed."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage($"{license}: does not exist."));
+            }
+        }
+
+        // Method to get the police station of the city
+        public PoliceStation GetPoliceStation()
+        {
+            return policeStation;
+        }
+
+        // Implement the IMessageWritter interface
+        public string WriteMessage(string customMessage)
+        {
+            return $"Taxi license {customMessage}";
+        }
+    }
 
 }
