ce64d0234aa5c134624a78fb794feae7e2d85a33
berordas <202214793@alu.comillas.edu>
2024-10-03 13:57:00 +0200

    actualizacion

---
+++ Scooter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+
+namespace Practice2
+{
+    class Scooter : Vehicle
+    {
+        private static string typeOfVehicle = "Scooter";
+
+        // Constructor
+        public Scooter(string color, string model) : base(typeOfVehicle, null)
+        {
+            this.Plate = $"{color} {model}";
+        }
+    }
+}
